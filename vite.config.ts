3f718001fa48ff74e77e382419372118ba7aef93
Irsyad A. Panjaitan <irsyadpanjaitan@gmail.com>
2023-01-11 22:45:25 +0700

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,6 @@ export default defineConfig({
         react(),
     ],
     ssr: {
-        noExternal: ['@inertiajs/server'],
+        noExternal: ['@inertiajs/server', '@tabler/icons'],
     },
 });
