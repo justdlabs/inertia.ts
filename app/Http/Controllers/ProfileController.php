bd8a90198ee3b627e18ece7734f621f37fe5cbab
Irsyad A. Panjaitan <irsyadpanjaitan@gmail.com>
2023-02-01 20:57:30 +0700

    This repo will work if Laravel is v10

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -37,7 +37,7 @@ class ProfileController extends Controller
 
         $request->user()->save();
 
-        return Redirect::route('profile.edit');
+        return to_route('profile.edit');
     }
 
     /**
