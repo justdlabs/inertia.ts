4b9d7e6c45466c9f9d9652a9cc452d2dde1bd9ec
Irsyad A. Panjaitan <irsyadpanjaitan@gmail.com>
2023-06-30 21:47:27 +0700

    fix port ssr

--- resources/js/ssr.tsx
+++ resources/js/ssr.tsx
@@ -6,23 +6,25 @@ import route from '../../vendor/tightenco/ziggy/dist/index.m';
 
 const appName = 'Laravel';
 
-createServer((page) =>
-    createInertiaApp({
-        page,
-        render: ReactDOMServer.renderToString,
-        title: (title) => `${title} - ${appName}`,
-        resolve: (name) => resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx')),
-        setup: ({ App, props }) => {
-            // @ts-expect-error
-            global.route = (name, params, absolute) =>
-                route(name, params, absolute, {
-                    // @ts-expect-error
-                    ...page.props.ziggy,
-                    // @ts-expect-error
-                    location: new URL(page.props.ziggy.location),
-                });
+createServer(
+    (page) =>
+        createInertiaApp({
+            page,
+            render: ReactDOMServer.renderToString,
+            title: (title) => `${title} - ${appName}`,
+            resolve: (name) => resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx')),
+            setup: ({ App, props }) => {
+                // @ts-expect-error
+                global.route = (name, params, absolute) =>
+                    route(name, params, absolute, {
+                        // @ts-expect-error
+                        ...page.props.ziggy,
+                        // @ts-expect-error
+                        location: new URL(page.props.ziggy.location),
+                    });
 
-            return <App {...props} />;
-        },
-    })
+                return <App {...props} />;
+            },
+        }),
+    13715
 );
