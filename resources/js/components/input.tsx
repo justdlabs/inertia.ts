20f70ae5430511db641184e7b2dae033e798250b
Irsyad A. Panjaitan <irsyadpanjaitan@gmail.com>
2023-07-23 21:50:53 +0700

    add rounded lg to input

--- resources/js/components/input.tsx
+++ resources/js/components/input.tsx
@@ -9,7 +9,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type,
         <input
             type={type}
             className={cn(
-                'flex h-11 w-full rounded-md border border-input bg-background px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus:border-brand focus-visible:outline-none focus-visible:ring focus-visible:ring-brand/10 disabled:cursor-not-allowed disabled:opacity-50',
+                'flex h-11 w-full rounded-lg border border-input bg-background px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus:border-brand focus-visible:outline-none focus-visible:ring focus-visible:ring-brand/10 disabled:cursor-not-allowed disabled:opacity-50',
                 className
             )}
             ref={ref}
