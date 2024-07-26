1c3fcc0cf9427cf003dc8c16a1bc0ddd9bdfdbfa
Irsyad A. Panjaitan <irsyad@parsinta.com>
2024-07-26 14:43:22 +0700

    remove all use client flag

--- resources/js/components/ui/avatar.tsx
+++ resources/js/components/ui/avatar.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import React from 'react'
 
 import { tv, type VariantProps } from 'tailwind-variants'
--- resources/js/components/ui/grid.tsx
+++ resources/js/components/ui/grid.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import { GridList, GridListItem, GridListItemProps, GridListProps } from 'react-aria-components'
 import { tv, type VariantProps } from 'tailwind-variants'
 
