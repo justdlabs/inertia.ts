32f5557c669f30936fbfaca46e488185540d81ad
Irsyad A. Panjaitan <irsyad@parsinta.com>
2024-07-29 08:23:22 +0700

    wip

--- resources/js/components/ui/dialog.tsx
+++ resources/js/components/ui/dialog.tsx
@@ -2,15 +2,15 @@ import { Dialog as DialogPrimitive, type DialogProps } from 'react-aria-componen
 import { twMerge } from 'tailwind-merge'
 
 const Dialog = ({ className, ...props }: DialogProps) => {
-    return (
-        <DialogPrimitive
-            {...props}
-            className={twMerge(
-                'relative max-h-[inherit] overflow-y-auto p-4 focus:outline-none outline-0 [[data-placement]>&]:p-4 dlc',
-                className
-            )}
-        />
-    )
+  return (
+    <DialogPrimitive
+      {...props}
+      className={twMerge(
+        'relative max-h-[inherit] overflow-y-auto p-4 focus:outline-none outline-0 [[data-placement]>&]:p-4 dlc',
+        className
+      )}
+    />
+  )
 }
 
 export { Dialog }
