2ec750c5157d4fd01f35846a85bc4dba1ff83b93
Irsyad A. Panjaitan <irsyad@parsinta.com>
2024-10-22 17:19:20 +0700

    lfg

---
+++ resources/js/components/ui/container.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { tv } from 'tailwind-variants';
+
+const containerStyles = tv({
+    base: 'mx-auto w-full max-w-7xl',
+    variants: {
+        variant: {
+            constrained: 'container sm:px-6 lg:px-8',
+            'padded-content': 'px-4 sm:px-6 lg:px-8'
+        }
+    },
+    defaultVariants: {
+        variant: 'padded-content'
+    }
+});
+
+interface ContainerProps extends React.HTMLAttributes<HTMLDivElement> {
+    variant?: 'constrained' | 'padded-content';
+}
+
+const Container = React.forwardRef<HTMLDivElement, ContainerProps>(({ className, variant, ...props }, ref) => (
+    <div className={containerStyles({ variant, className })} {...props} ref={ref} />
+));
+
+Container.displayName = 'Container';
+
+export { Container };
